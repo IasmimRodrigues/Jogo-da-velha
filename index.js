5add053f7c46d75e4cea0d0040106a3dc0b43b95
Iasmim Rodrigues <iasmimrodrigues1301@gmail.com>
2023-05-01 00:05:46 -0300

    Funcionando com dois jogadores

--- index.js
+++ index.js
@@ -1,26 +1,79 @@
 const cellElements = document.querySelectorAll("[data-cell]");
+const jogada = document.querySelector("jogada");
+let turno;
 
 let isCircleTurn = false;
 
+const winningCombinations = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+
+const endGame = (isDraw) => {
+  if (isDraw) {
+    window.location = "./empatou.html";
+  } else {
+    if (isCircleTurn == true) {
+        window.location = "./ovenceu.html";
+    } else {
+        window.location = "./xvenceu.html";
+    }
+  }
+};
+
+const checkForWin = (currentPlayer) => {
+  return winningCombinations.some((combination) => {
+    return combination.every((index) => {
+      return cellElements[index].classList.contains(currentPlayer);
+    });
+  });
+};
+
+const checkForDraw = () => {
+  return [...cellElements].every((cell) => {
+    return cell.classList.contains("X") || cell.classList.contains("O");
+  });
+};
+
 const placeMark = (cell, classToAdd) => {
-    cell.innerHTML = classToAdd;
-}
+  // adiciona X e O
+  cell.innerHTML = classToAdd;
+  cell.classList.add(classToAdd);
+};
 
 const swapTurns = () => {
-    isCircleTurn = !isCircleTurn;
-}
+  // troca de turno
+  isCircleTurn = !isCircleTurn; //inverte o turno
+};
 
 const handleClick = (e) => {
-    const cell = e.target;
-    const classToAdd = isCircleTurn ? '<h2> O </h2>' : '<h2> X </h2>';
+  // vê de quem é a vez
+  const cell = e.target;
+  const classToAdd = isCircleTurn ? "O" : "X";
+  
 
-    // cell.add(classToAdd);
-    
-    placeMark(cell, classToAdd);
+  placeMark(cell, classToAdd);
+
+  const isWin = checkForWin(classToAdd);
+
+  const isDraw = checkForDraw();
+  console.log(isCircleTurn);
+
+  if (isWin) {
+    endGame(false);
+  } else if (isDraw) {
+    endGame(true);
+  } else {
     swapTurns();
-}
+  }
+};
 
 for (const cell of cellElements) {
-    cell.addEventListener('click', handleClick, {once: true});
+  cell.addEventListener("click", handleClick, { once: true });
 }
-
